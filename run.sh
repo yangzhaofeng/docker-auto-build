3ee479cf4deb1c4125a08bb172dcdc4dd86e9be3
楊肇峰 <yangzhaofengsteven@gmail.com>
2018-12-20 22:58:31 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+curl -X POST $DANTED
+curl -X POST $ALIYUN-DDNS
+curl -X POST $HIDDEN-SFTP
+curl -X POST $STRONGSWAN
+curl -X POST $BIND9
+curl -X POST $OCSERV
+curl -X POST $OPENVPN
+#curl -X POST $OPENVPN-SOCKS
+#curl -X POST
+#curl -X POST
+#curl -X POST
+#curl -X POST
+#curl -X POST
+#curl -X POST
+#curl -X POST
+#curl -X POST
+#curl -X POST
+#curl -X POST
