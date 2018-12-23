4d099e3f6946a2af0ff9205bf1371fc62cbb5bbd
Steven Yang <yangzhaofengsteven@gmail.com>
2018-12-23 20:28:04 +0800

    add openvpn-socks to docker hub build

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ curl -X POST $STRONGSWAN
 curl -X POST $BIND9
 curl -X POST $OCSERV
 curl -X POST $OPENVPN
-#curl -X POST $OPENVPN-SOCKS
+curl -X POST $OPENVPN_SOCKS
 #curl -X POST
 #curl -X POST
 #curl -X POST
