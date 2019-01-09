d4dbeee6c7d04a32c07dad544d019c4b4732f128
Steven Yang <yangzhaofengsteven@gmail.com>
2019-01-09 16:55:58 +0800

    test

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-curl -X POST $DANTED
-curl -X POST $ALIYUN_DDNS
-curl -X POST $HIDDEN_SFTP
-curl -X POST $STRONGSWAN
-curl -X POST $BIND9
-curl -X POST $OCSERV
-curl -X POST $OPENVPN
-curl -X POST $OPENVPN_SOCKS
+#curl -X POST $DANTED
+#curl -X POST $ALIYUN_DDNS
+#curl -X POST $HIDDEN_SFTP
+#curl -X POST $STRONGSWAN
+#curl -X POST $BIND9
+#curl -X POST $OCSERV
+#curl -X POST $OPENVPN
+#curl -X POST $OPENVPN_SOCKS
 #curl -X POST
 #curl -X POST
 #curl -X POST
